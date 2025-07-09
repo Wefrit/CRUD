b1bd60692523871eb5d0f21764af583efc2c2cab
Wefrit <lopes.nathanlemes@gmail.com>
2025-07-09 15:02:27 -0300

    Altera a view usando template; E cria uma classe model com title, description, created_at, deadline e finished_at

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=100, blank=False, null=False)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=False, null=False)
+    deadline = models.DateTimeField(blank=False, null=False)
+    finished_at = models.DateTimeField(null=True)
